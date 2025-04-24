21e59eb714fdb4f3254115551e99f1d2f1a2da09
ashwin e <ashwine2k02@gmail.com>
2025-04-24 14:07:58 +0530

    Prepare

--- taskproject/urls.py
+++ taskproject/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.http import HttpResponse
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+import tasks
 from tasks.views import TaskViewSet
 
 router = DefaultRouter()
@@ -24,5 +26,6 @@ router.register(r'tasks', TaskViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include(router.urls)),
+    path('api/', include(tasks.urls)),
+    path('', lambda request: HttpResponse("✅ API is working! Visit /admin or /api/tasks")),
 ]
