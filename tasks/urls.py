3ca0522f4fea71cc3dbdc2cf66a51314ef9b9765
ashwin e <ashwine2k02@gmail.com>
2025-04-24 17:20:09 +0530

    7

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,6 +1,8 @@
+# tasks/urls.py
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('tasks/', views.get_or_create_tasks),  # This should be the endpoint to create or get tasks
+    path('', views.get_or_create_tasks),  # 👈 this matches /api/
+    path('tasks/', views.get_or_create_tasks),
 ]
