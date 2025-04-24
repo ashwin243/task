0f711730392ea9623c25bd388f882a7a81c1dba9
ashwin e <ashwine2k02@gmail.com>
2025-04-24 17:16:22 +0530

    6

--- taskproject/urls.py
+++ taskproject/urls.py
@@ -5,5 +5,5 @@ from django.views.generic import RedirectView  # 👈 make sure this is imported
 urlpatterns = [
     path('admin/', admin.site.urls),                 # Admin panel
     path('api/', include('tasks.urls')),             # API route
-    path('', RedirectView.as_view(url='/api/tasks/', permanent=False)),  # 👈 Redirect root URL
+    path('', RedirectView.as_view(url='/api/', permanent=False)),  # 👈 Redirect root URL
 ]
