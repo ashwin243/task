eebecc272761d69910d7313b6fb96381765bbee7
ashwin e <ashwine2k02@gmail.com>
2025-04-24 14:53:12 +0530

    nooo

--- tasks/urls.py
+++ tasks/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.task_list),  # or whatever your view is
+    path('tasks/', views.get_tasks),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,14 +1,10 @@
-from django.shortcuts import render
-
-# Create your views here.
-from rest_framework import viewsets
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from .models import Task
 from .serializers import TaskSerializer
 
-class TaskViewSet(viewsets.ModelViewSet):
-    queryset = Task.objects.all()
-    serializer_class = TaskSerializer
-from django.http import JsonResponse
-
-def task_list(request):
-    return JsonResponse({'tasks': []})
+@api_view(['GET'])
+def get_tasks(request):
+    tasks = Task.objects.all()
+    serializer = TaskSerializer(tasks, many=True)
+    return Response(serializer.data)
