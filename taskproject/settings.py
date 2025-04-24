0c380aea3e301d665d651b3bfeee71eafaf9fc6c
ashwin e <ashwine2k02@gmail.com>
2025-04-24 15:13:49 +0530

    debug

--- taskproject/settings.py
+++ taskproject/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!y9u2ig-rl7j-u54))15sbr0rg14x$z1^*w(rr&lz8yi$pekrs'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
